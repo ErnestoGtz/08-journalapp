7aa127af03558886f083ab24cf54c71af207a37c
Ernesto Emmanuel Gutierrez Muñoz <ernesto.gtz@itcoach.com.mx>
2020-08-21 08:36:45 -0500

    CRUD

--- src/types/types.js
+++ src/types/types.js
@@ -1,8 +1,18 @@
 export const types = {
     login:'[Auth] Login',
     logout:'[Auth] Logout',
+    
     uiSetError:'[UI] Set Error',
     uiRemoveError:'[UI] Remove Error',
     uiStartLoading:'[UI] Start Loading',
-    uiFinishLoading:'[UI] Finish Loading'
+    uiFinishLoading:'[UI] Finish Loading',
+    
+    notesAddNew:'[Notes] New Note',
+    notesActive:'[Notes] Set active note',
+    notesLoad:'[Notes] Load notes',
+    notesUpdated:'[Notes] Update note',
+    notesFileUrl:'[Notes] Update image',
+    notesDelete:'[Notes] Delete note',
+    notesLogoutCleaning:'[Notes] Logout cleaning'
+
 }
